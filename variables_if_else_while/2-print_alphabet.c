74c910c65450ab53412267d3971f449eba937cc4
Iglcyril <iglcyril@hotmail.fr>
2025-10-22 12:41:06 +0000

    github account change

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - entry point
+*Return : always 0 (success)
+ */
+
+ int main(void)
+ { 
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return(0);
+	
+ }
