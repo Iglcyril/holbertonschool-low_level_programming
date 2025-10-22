37fa3fdb686d862daa7f32f3b25de317490ace73
Iglcyril <iglcyril@hotmail.fr>
2025-10-22 17:20:04 +0000

    feat : create program to print all letter in lowercase and uppercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - entry point
+*Return: always 0 (success)
+ */
+ int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter ++)
+		putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
