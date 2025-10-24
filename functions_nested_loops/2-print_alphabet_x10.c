e174a77e8f85b13e462aaa849e2742dea9c7bd7f
Iglcyril <iglcyril@hotmail.fr>
2025-10-24 18:36:45 +0000

    feat: add print_alphabet_x10 function and main program

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Return : Always : 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char a;
+
+	while (count < 10)
+	{
+		a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
