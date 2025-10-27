8725077d2f1f510807248c53cda02e0d1911780d
Iglcyril <iglcyril@hotmail.fr>
2025-10-27 22:09:25 +0000

    fix: rename function from print_line to print_diagonal and update comments for clarity

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,26 +1,23 @@
 #include "main.h"
 /**
- * print_line - Entry point
+ * print_diagonal - Entry point
  *@n : number of time character _ repeat
  * Return : Always : 0 (Success)
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int i, j;
 
 	if (n <= 0)
 	{
-		for (i = 0; i < n; i++)
-			_putchar(' ');
-		for (j )
-
-
 		_putchar('\n');
-
-		for (i = 0; i < n; i++)
-		{
-		_putchar ('_');
-		}
-		_putchar ('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
