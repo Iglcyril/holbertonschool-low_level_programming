b957d633615b34287ad3beb3f67c92556a556c68
Iglcyril <iglcyril@hotmail.fr>
2025-10-27 11:14:40 +0000

    feat: add print_numbers and _putchar functions with corresponding header updates

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_numbers - Entry point
+ * @c: int char
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	_putchar('0');
+	_putchar('1');
+	_putchar('2');
+	_putchar('3');
+	_putchar('4');
+	_putchar('5');
+	_putchar('6');
+	_putchar('7');
+	_putchar('8');
+	_putchar('9');
+	_putchar ('\n');
+}
