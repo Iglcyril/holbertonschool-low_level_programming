7c74898017b24a122aaf8af58e504aff643e4441
Iglcyril <iglcyril@hotmail.fr>
2025-10-27 13:53:17 +0000

    feat: add print_diagonal function and update header file

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ *@n : number of time character _ repeat
+ * Return : Always : 0 (Success)
+ */
+void print_line(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar(' ');
+		for (j )
+
+
+		_putchar('\n');
+
+		for (i = 0; i < n; i++)
+		{
+		_putchar ('_');
+		}
+		_putchar ('\n');
+	}
+}
