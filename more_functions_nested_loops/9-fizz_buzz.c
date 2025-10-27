2a9b51c11f6b545e5b75587e4163a1be74e42cac
Iglcyril <iglcyril@hotmail.fr>
2025-10-27 21:15:38 +0000

    feat: implement fizz_buzz function to print numbers with FizzBuzz logic

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,74 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @a: The character to print
+ *
+ * Return: On success 1.
+ * 
+ */
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void print_number(int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar('0' + (n % 10));
+}
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (a % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (a % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else
+		{
+			print_number(a);
+		}
+
+		if (a != 100)
+			_putchar(' ');
+
+	}
+	_putchar('\n');
+	return (0);
+}
