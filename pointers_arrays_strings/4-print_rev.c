31add96a95a723aca2d134967eac31ca85594dbb
Iglcyril <iglcyril@hotmail.fr>
2025-10-28 14:41:53 +0000

    feat :create to print reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+* str - entry point
+* @a : pointer
+* Return (0)
+*/
+void print_rev(char *s)
+{
+int a = 0;
+
+	while (s[a] != '\0')
+		a++;
+
+
+	while (a >= 0)
+	{
+		_putchar(s[a]);
+		a--;
+	}
+
+
+_putchar('\n');
+}
