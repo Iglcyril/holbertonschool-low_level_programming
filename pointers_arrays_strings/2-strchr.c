8f467f069c37bc5fd1de3adc04d7c005981b33c3
Iglcyril <iglcyril@hotmail.fr>
2025-11-03 11:59:31 +0000

    feat : function that locates a character in a string.

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locate a character in the string
+ * @s: pointer
+ * @c: character
+ * Return: NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+}
