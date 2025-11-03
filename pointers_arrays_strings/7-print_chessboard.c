11d46886b24d9405caf7597f8d0426378c0479ac
Iglcyril <iglcyril@hotmail.fr>
2025-11-03 13:55:58 +0000

    feat : function that prints the chessboard.

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - print the chessboard
+ * @a: 8x8 cheesboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+		_putchar('\n');
+	}
+}
