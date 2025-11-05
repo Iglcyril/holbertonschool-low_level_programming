fef2330fcdac3f32941de018ac5c3b986bb50869
Iglcyril <iglcyril@hotmail.fr>
2025-11-05 12:50:33 +0000

    feat : function that returns the value of x raised to the power of y.

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _pow_recursion - return x raised to the power of y
+ * @x: number
+ * @y: exponent
+ * Return: result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
+
