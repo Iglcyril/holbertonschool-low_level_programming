9d0b8be735da82cf64a959fa0d7613e0dc05466a
Iglcyril <iglcyril@hotmail.fr>
2025-11-05 10:57:51 +0000

    feat : Write a function that prints a string in reverse.

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_ rev_recursion - prints a string*
+ * @s: string
+ * Return
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+
+}
