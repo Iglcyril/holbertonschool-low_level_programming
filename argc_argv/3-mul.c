3f1a52a1f46f238eb5bf61f8709ffbfc84846bc9
Iglcyril <iglcyril@hotmail.fr>
2025-11-07 13:25:34 +0000

    feat : correction program

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,27 +1,26 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * main - multiple two numbers
  * @argc: argument count
- * @argv: argument
- * Return: 0 to succes and 1 for error
+ * @argv: argument vector
+ * Return: 0 to succes, 1 for Error
  */
 int main(int argc, char *argv[])
 {
-	int x;
-	int y;
-	int result;
+	int x, y, result;
 
 	if (argc != 3)
 	{
-		printf("error\n");
+		printf("Error\n");
 		return (1);
 	}
 
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
-
 	result = x * y;
+
 	printf("%d\n", result);
 
 	return (0);
