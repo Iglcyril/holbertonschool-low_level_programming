1fdf57272be50f1ceabefa7e7f6921f02c4beddf
Iglcyril <iglcyril@hotmail.fr>
2025-11-17 12:50:05 +0000

    feat :  function that executes a function given as a parameter on each element of an array.

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * array_iterator - execute a function
+ * @array: pointer to the array
+ * @size: number of elements in the array
+ * @action: pointer to the fonction
+ * Description: if array or action is NULL, nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
