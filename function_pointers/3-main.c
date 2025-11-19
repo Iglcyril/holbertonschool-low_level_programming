f223d04b032d76d6f5a498c30224fa486dbd583f
Iglcyril <iglcyril@hotmail.fr>
2025-11-19 08:47:41 +0000

    feat : main with argument and error

---
+++ function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - calculator
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success and 98/99/100 on Error
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", operation(num1, num2));
+	return (0);
+}
