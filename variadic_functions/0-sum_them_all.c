b1df94e52764807aaad5d5b89afc004c8e72c2bd
Iglcyril <iglcyril@hotmail.fr>
2025-11-19 14:07:13 +0000

    fix : modification code

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,18 +2,22 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- * 
+ * sum_them_all - return the sum of all
+ * @n: number of arguments
+ * ...: variadic
+ * Return: sum of integers
+ * if n == 0, return 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int *operation;
+	unsigned int operation;
 	va_list ap;
+	int sum = 0;
 
 	va_start(ap, n);
-
-	printf("%d", sum_them_all);
-
-	operation = va_arg(ap, unsigned int	*);
+	for (operation = 0; operation < n; operation++)
+		sum += va_arg(ap, int);
 
 	va_end(ap);
+	return (sum);
 }
