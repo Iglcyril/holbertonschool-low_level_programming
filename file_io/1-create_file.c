ba2996d96f9b6d207880fa49514121677191d751
Iglcyril <iglcyril@hotmail.fr>
2025-12-05 22:58:55 +0000

    fix: code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,38 +1,39 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
 /**
- * create_file - creates a file
+ * create_file - creates a file with specific content
  * @filename: name of the file to create
- * @text_content: content to write in the file
+ * @text_content: NULL terminated string to write to the file
+ *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-
-	int fic;
-	ssize_t write_resu;
-	int len = 0;
+	int fd, len = 0;
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (-1);
 
-	fic = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fic == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		close(fic);
-		return (1);
+		while (text_content[len])
+			len++;
+
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-	while (text_content[len])
-		len++;
 
-	write_resu = write(fic, text_content, len);
-
-	close(fic);
-
-	if (write_resu == -1)
-		return (-1);
-
+	close(fd);
 	return (1);
 }
