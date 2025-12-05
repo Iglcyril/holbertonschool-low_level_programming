b6a988b5cf6f329a1bed5a2d3558cf13f15cecb9
Iglcyril <iglcyril@hotmail.fr>
2025-12-05 22:51:12 +0000

    fix : implify variable declarations and close handling in create_file function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fic = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fic = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fic == -1)
 		return (-1);
 
@@ -24,16 +24,14 @@ int create_file(const char *filename, char *text_content)
 		close(fic);
 		return (1);
 	}
-	while (text_content[len] != '\0')
+	while (text_content[len])
 		len++;
 
 	write_resu = write(fic, text_content, len);
+
+	close(fic);
+
 	if (write_resu == -1)
-	{
-		close(fic);
-		return (-1);
-	}
-	if (close(fic) == -1)
 		return (-1);
 
 	return (1);
